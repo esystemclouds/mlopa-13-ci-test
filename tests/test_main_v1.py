4379d3703c73f360a0709d6c1fa69a65b38856ef
Eliecer Canales <ecanales@esystem.cl>
2025-12-06 11:53:59 -0300

    test2

--- tests/test_main_v1.py
+++ tests/test_main_v1.py
@@ -3,7 +3,7 @@ from my_app.main_v1_1 import add
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1, 2), 3) 
+        self.assertEqual(add(1, 2), 4) 
         self.assertEqual(add(-1, 1), 0) 
         self.assertEqual(add(0, 0), 0)
         
